37ce6b49936ba08c01d76875c02f33c17f136c80
dylan <dylangthomas1@gmail.com>
2022-10-14 21:43:28 -0600

    colors display

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-ECHO is on.
+const start = document.getElementById('start');
+const scoreDisplay = document.getElementById('scoreDisplay');
+const cells = document.querySelectorAll('.cell');
+
+let arr = ['red', 'green', 'blue', 'yellow'];
+let answer = [];
+
+start.addEventListener('click', startGame);
+function startGame() {
+    start.style.display = 'none';
+    scoreDisplay.style.display = 'block';
+    round();
+}
+
+function round() {
+    let color = arr[Math.floor(Math.random() * arr.length)];
+    answer.push(color);
+    displayColor();
+}
+
+function displayColor() {
+    let i = 0;
+    while(i < answer.length) {
+        let theCell = answer[i];
+        switch(theCell) {
+            case 'red':
+                document.getElementById('red').style.backgroundColor = '#F5473F';
+                break;
+            case 'green':
+                document.getElementById('green').style.backgroundColor = '#5CE160';
+                break;
+            case 'blue':
+                document.getElementById('blue').style.backgroundColor = '#7C92D9';
+                break;
+            case 'yellow':
+                document.getElementById('yellow').style.backgroundColor = '#DBEA91';
+                break;
+        }
+        setTimeout(() => {
+            document.getElementById('red').style.backgroundColor = 'red';
+            document.getElementById('green').style.backgroundColor = 'green';
+            document.getElementById('blue').style.backgroundColor = 'blue';
+            document.getElementById('yellow').style.backgroundColor = 'yellow';
+        }, 1500)
+        i++;
+    }
+    cells.forEach(cell => {
+        cell.addEventListener('click', guess);
+    })
+}
+
+function guess() {
+    
+}
